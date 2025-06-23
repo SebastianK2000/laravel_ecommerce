53bf1ea12ec6c88481f4b0c1f7e1afdef1d5f556
Sebastian <sebastian.kosciolek2000@gmail.com>
2025-06-23 22:10:14 +0200

    add layout and component

--- Laravel/ecommerce/resources/views/home/index.blade.php
+++ Laravel/ecommerce/resources/views/home/index.blade.php
@@ -1,15 +1,34 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<!DOCTYPE html>
+<html>
 
-<h1>HOME</h1>
+<head>
+
+    @include('home.css')
+
+</head>
+
+<body>
+<div class="hero_area">
+    <!-- header section strats -->
+    @include('home.header')
+    <!-- end header section -->
+    <!-- slider section -->
+    @include('home.slider')
+    <!-- end slider section -->
+</div>
+<!-- end hero area -->
+
+<!-- shop section -->
+@include('home.product')
+<!-- end shop section -->
+
+
+<!-- contact section -->
+@include('home.contact')
+<!-- end contact section -->
+<!-- info section -->
+@include('home.footer')
 
 </body>
+
 </html>
